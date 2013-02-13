9c2bd55e8fea151db902301e84651999e733281f
Lucas Medeiros <lucastoc@gmail.com>
2013-02-13 13:53:03 -0300

    adding ActiveTouch.h file

---
+++ ActiveTouchExample/ActiveTouch/ActiveTouch.h
@@ -0,0 +1,12 @@
+//
+//  ATDatabaseCleaner.h
+//  ActiveTouchExample
+//
+//  Created by Lucas Medeiros on 10/02/13.
+//  Copyright (c) 2013 ActiveTouch. All rights reserved.
+//
+
+#import "ATDatabaseContainer.h"
+#import "ATModel.h"
+#import "ATDatabaseTestRunner.h"
+#import "ATBeValidMatcher.h"
